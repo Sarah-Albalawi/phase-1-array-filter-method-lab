33b57fa7c0002431a2a27336b472d51d629f3920
Sarah-Albalawi <s.albalawi@outlook.com>
2022-11-03 10:12:40 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(str, driver) {
+  return str.filter(
+    (driverMatch) => driverMatch.toLowerCase() === driver.toLowerCase()
+  );
+}
+
+function fuzzyMatch(str, testStr) {
+  return str.filter(
+    (driverMatch) =>
+      driverMatch.toLowerCase().indexOf(testStr.toLowerCase()) === 0
+  );
+}
+
+function matchName(str, driverName) {
+  return str.filter((record) => record.name === driverName);
+}
